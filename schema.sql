c3166c473ae9a3fad8ecefbce4b8ebfa1d00573b
James <andritjm@gmail.com>
2023-03-09 19:57:38 -0600

    added schema.sql

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE database emp_db;
+USE emp_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL
+),
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL
+);
